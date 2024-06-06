d8afa379b46c0e3193682a87f7922102b7fa657a
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-06 23:11:30 +0530

    Add profanity threshold and whitelisted words

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const PROFANITY_THRESHOLD = 0.87;
+
+// there is bias in the underlying open-source embedding models
+// so these are flagged inappropriately
+export const WHITELIST = ["black", "swear"];
